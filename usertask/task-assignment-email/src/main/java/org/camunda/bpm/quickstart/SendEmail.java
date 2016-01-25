206c4212c59c83df0568554c65c33edd346ccb9f
Makarenkov, Deniss / Kuehne + Nagel / TLL MI-AP <deniss.makarenkov@kuehne-nagel.com>
2016-01-25 11:37:13 +0200

    initial java class commit

---
+++ usertask/task-assignment-email/src/main/java/org/camunda/bpm/quickstart/SendEmail.java
@@ -0,0 +1,34 @@
+
+//import kn.ofs.user.integration.ws.email.CIEL;
+//import kn.ofs.user.integration.ws.email.CIELSoap;
+import org.
+
+import javax.xml.namespace.QName;
+import java.net.URL;
+import java.util.logging.Logger;
+
+public class SendEmail {
+
+//    private static Logger log = Logger.getLogger(SendEmail.class.getName());
+//
+//    private static final QName SERVICE_NAME = new QName("http://w3docfis.ts.int.kn/KN-eScan/", "CIEL");
+//
+//    public void send(String senderAddress, String receiverAddress, String subject, String body) throws Exception {
+//
+//        URL wsdlURL = new URL("http://dwh-ftp.int.kn/KN-eScan/CIEL.asmx?WSDL");
+//
+//        CIEL ss = new CIEL(wsdlURL, SERVICE_NAME);
+//        CIELSoap port = ss.getCIELSoap();
+//
+//        boolean useHTML = true;
+//        byte[] attachment = new byte[0];
+//
+//        String attachmentName = "";
+//
+//        boolean sendSMTPReturn = port.sendSMTP(senderAddress, receiverAddress, subject, body, useHTML, attachment, attachmentName);
+//
+//        log.info("SendSMTP result: " + sendSMTPReturn);
+//    }
+
+
+}
